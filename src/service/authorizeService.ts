f21073c5a2468ce3a5bc4d1dca4f22197d0baa94
Nastassia Fiadotava <nastassia_fiadotava@epam.com>
2021-05-14 09:51:09 +0300

    feat: add method findUser

--- src/service/authorizeService.ts
+++ src/service/authorizeService.ts
@@ -1,6 +1,7 @@
 import { v4 } from "uuid";
 import User from "../models/users";
 import Session, { Sessions } from "../models/sessions";
+
 async function logIn(login: string, password: string): Promise<string> {
   try {
     const user = await User.findOne({
@@ -22,12 +23,12 @@ async function logIn(login: string, password: string): Promise<string> {
   }
 }
 
-async function create(id: string): Promise<string> {
+async function create(userId: string): Promise<string> {
   try {
     const session: Sessions = {
       id: v4(),
       date: new Date(),
-      user_id: id
+      user_id: userId
     }
     const newSession = await Session.create(session);
 
@@ -37,9 +38,28 @@ async function create(id: string): Promise<string> {
   }
 }
 
+async function findUserId(sessionId: string): Promise<string> {
+  try {
+    const session = await Session.findOne({
+      where: {
+        id: sessionId
+      }, raw: true
+    });
+
+    if (session) {
+      return session.user_id
+    }
+
+    throw new Error("Not found");
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 const service = {
   logIn,
-  create
+  create,
+  findUserId
 }
 
 export default service;
