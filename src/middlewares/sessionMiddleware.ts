82f914ef95d9ed7c548afee01cbffe361d73b4f0
Nastassia Fiadotava <nastassia_fiadotava@epam.com>
2021-05-14 11:36:38 +0300

    feat: add validate date session

--- src/middlewares/sessionMiddleware.ts
+++ src/middlewares/sessionMiddleware.ts
@@ -3,7 +3,14 @@ import service from './../service/authorizeService'
 const session = async function (req, _res, next): Promise<void> {
   try {
     const sessionId = req.cookies.session;
-    const userId = await service.findUserId(sessionId);
+    const session = await service.find(sessionId)
+    const userId = session.user_id;
+    const date = new Date();
+
+    if (date > session.date) {
+      await service.remove(sessionId);
+      throw new Error('Session timed out');
+    }
 
     req.auts = {
       userId: userId
